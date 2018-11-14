a4899aa76b41551936e70b1afd39e96ea062ff71
Lukas Rehor <lukas.rehor@email.cz>
2018-11-14 15:06:18 +0100

    Revert "Revert "cob-nnectWithMySQL app done""
    This reverts commit b46f07ff9ba50d938f11d2673d8266aa9bd872ac.

--- week-08/day-2/ConnectionWithMySQL/src/main/java/com/greenfox/connectionwithmysql/model/Todo.java
+++ week-08/day-2/ConnectionWithMySQL/src/main/java/com/greenfox/connectionwithmysql/model/Todo.java
@@ -37,4 +37,20 @@ public class Todo {
     public boolean isDone() {
         return done;
     }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setUrgent(boolean urgent) {
+        this.urgent = urgent;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
 }
